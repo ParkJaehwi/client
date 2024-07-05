0573f75f29d86c001806cac94e07681b0d206a0b
ParkJaeHwi <2606pjh@naver.com>
2024-07-05 10:40:14 +0900

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, Button } from "react";
 
 function App() {
+  const button = () => {
+    fetch("http://localhost:5000/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+  const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/api/data")
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data));
+  // }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>시발련zsddfasdfafsadfz</p>
+
+      <button onClick={button}>버튼임</button>
+      <p>임시텍스트</p>
+      <header className="App-header">{data ? <p>{data.message}</p> : <p>Loading...</p>}</header>
     </div>
   );
 }
